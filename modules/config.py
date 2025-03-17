8638206add0f72fad584b2fa37378839829c0fe3
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2025-03-17 16:45:32 +0000

    8 locations with qml

--- modules/config.py
+++ modules/config.py
@@ -11,7 +11,7 @@ ENCODING = 'utf-8-sig'              # Encoding of csv file
 
 # configuration files loaded from control.csv in production runs
 LOCATIONS = 8                       # number of locations to be visited
-SLICES = [1, 0.75]                      
+SLICES = [1, 0.75, 0.6, 0.5, 0.4, 0.25, 0.15, 0.05]                     
                                     # Slices to use when calculating the gradient
                                     #[1, 0.75, 0.6, 0.5, 0.4, 0.25, 0.15, 0.05] 
                                     # For example, 0.2 means that the best 20% 
@@ -20,7 +20,7 @@ SHOTS = 1024                        # shots used for each call of the quantum ci
 MODE = 2                            # MODE = 1 - rxgate, rygate, cnot gates
                                     # MODE = 2 - rxgate, XX gates -can be used with Hot Start
                                     # MODE = 9 - Pytorch model
-ITERATIONS = 5                      # updates, or iterations
+ITERATIONS = 1000                   # updates, or iterations
 
 GRAY = True                         # Use Gray codes
 HOT_START = True                    # Make a hot start
@@ -29,7 +29,7 @@ GRADIENT_TYPE = 'SPSA'              # controls the optimiser used
                                     # 'parameter_shift' - default
                                     # 'SPSA' is a stochastic gradient descent
 
-DECODING_FORMULATION = 'original'   # 'original' or 'new' - new is forumlation from paper
+DECODING_FORMULATION = 'original'   # 'original' or 'new' - new is formulation from paper
 
 ALPHA = 0.602                       # constant that controls the learning rate for SPSA decays
 BIG_A = 50                          # A for SPSA
@@ -39,7 +39,7 @@ GAMMA = 0.101                       # constant that determines how quickly the S
 S = 0.5                             # parameter for parameter shift.  Default is 0.5
 
 VERBOSE = False                      # controls how much is printed
-PRINT_FREQUENCY = 10                # how often results are printed out
+PRINT_FREQUENCY = 100                # how often results are printed out
 CACHE_MAX_SIZE = 500_000             #maximum size of the cache.
 
 CHANGE_EACH_PARAMETER = True        # Iterate through each parameter in the circuit
@@ -47,6 +47,3 @@ PLOT_PARAMETER_EVALUATION = True    # Plot the evaluation of each parameter
 READ_AND_WRITE_DATA = True          # Read and write data to csv file  
 
 ROTATIONS = 100                     # number of rotations sampled in parameter graphs
-
-#Classical ML parameters
-QUANTUM = True
